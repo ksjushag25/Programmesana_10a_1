2a75ec7555fb2186e37bee2148d259ce4e5c10e6
ksjushag25 <79309657+ksjushag25@users.noreply.github.com>
2021-02-24 09:58:34 +0200

    Update 10a_1gr.py
    Pierakstiju formulu

--- 10a_1gr.py
+++ 10a_1gr.py
@@ -2,22 +2,22 @@
 
 2 Berga Laura
 
-3 Fiïimonovs Eldars
+3 FiÃ¯imonovs Eldars
 
-4 Gaiğute Ksenija
-
+4 GaiÃ°ute Ksenija
+print (6/2)
 5 Gorniks Arturs
 
-6 Guşonoka Jevgeòija
+6 GuÃ¾onoka JevgeÃ²ija
 
-7 Isaèenko Aleksandrs
+7 IsaÃ¨enko Aleksandrs
 
-8 Komağilo Dmitrijs
+8 KomaÃ°ilo Dmitrijs
 
-9 Lukjanèikova Darja
+9 LukjanÃ¨ikova Darja
 
 10 Minalto Jana
 
 11 Obrazcova Marija
 
-12 Ğvacka Marija
+12 Ãvacka Marija
