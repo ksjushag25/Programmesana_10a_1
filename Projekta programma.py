efcb0cbaacac39e23e4d6c2ae5b0638c628feb6d
KsenijaGaisute <79309657+ksjushag25@users.noreply.github.com>
2021-04-16 10:46:31 +0300

    Update Projekta programma.py

--- Projekta programma.py
+++ Projekta programma.py
@@ -1,29 +1,20 @@
 a = int(input("Cik daudz cilveku edis pirmo edienu? - "))
 i = float(input("Cik maksa sis ediens? (eiro) - "))
-q = int(input("Ievadiet skaitli - " ))
-if q>1:
-    print ("kopeja summa ir", (a*i)+20)
-else:
-    b = int(input("Cik cilveku edis otro edienu? - "))
-    t = float(input("Cik maksa sis ediens? (eiro) - "))
-    q = int(input("Ievadiet skaitli - " ))
-if q>1:
-    print ("kopeja summa ir", (a*i)+(b*t)+20)
-else:
-    c = int(input("Cik cilveku edis treso edienu? - "))
-    x = float(input("Cik maksa sis ediens? (eiro) - ")) 
-    q = int(input("Ievadiet skaitli - " ))
-if q>1:
-    print ("kopeja summa ir", (a*i)+(b*t)+(c*x)+20)
-else:
-    d = int(input("Cik cilveku edis cetro edienu? - "))
-    z = float(input("Cik maksa sis ediens? (eiro) - ")) 
-    q = int(input("Ievadiet skaitli - " ))
-if q>1:
-    print ("kopeja summa ir", (a*i)+(b*t)+(c*x)+(d*z)+20)
-else:
-    v = int(input("Cik cilveku edis piekto edienu? - "))
-    m = float(input("Cik maksa sis ediens? (eiro) - ")) 
-    q = int(input("Ievadiet skaitli - " ))
-if q>1:
-    print ("kopeja summa ir", (a*i)+(b*t)+(c*x)+(d*z)+(v*m)+20)
+
+b = int(input("Cik cilveku edis otro edienu? - "))
+t = float(input("Cik maksa sis ediens? (eiro) - "))
+
+c = int(input("Cik cilveku edis treso edienu? - "))
+x = float(input("Cik maksa sis ediens? (eiro) - ")) 
+
+pe = (a*i)+20
+oe = (b*t)+20
+te = (c*x)+20
+
+allmoney = (pe + oe + te)
+
+print("kopeja summa pirma ediena ir", pe)
+print("kopeja summa otra ediena ir", oe)
+print("kopeja summa tresa ediena ir", te)
+
+print("kopeja summa par visiem edieniem ir", allmoney)
